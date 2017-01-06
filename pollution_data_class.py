174f6a552c54ec7c4c8791231de8960067b99bc2
shubham <shubham0075@gmail.com>
2017-01-06 21:50:47 +0530

    added aqi data member

--- pollution_data_class.py
+++ pollution_data_class.py
@@ -3,15 +3,16 @@ import time
 
 #class definatons to be followed by all different wesbite scripts
 class pollution_data:
-	def __init__(self,location,pm25 = 0,pm10 = 0):
+	def __init__(self,location,pm25 = 0,pm10 = 0,aqi=-999):#adopting us embassy aqi default non functioning value equivalent as -999
 		self.location = location
 		self.data_time = time.time()
 		self.pm10 = pm10
 		self.pm25 = pm25
+		self.aqi = aqi
 	
 
 	def describe(self):
-		print self.location, self.data_time , self.pm25 , self.pm10
+		print self.location, self.data_time , self.pm25 , self.pm10 , self.aqi
 
 
 
