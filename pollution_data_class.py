5cb5caf92bfce6a28f7305ea800c596da8b364c2
shubham <shubham0075@gmail.com>
2017-01-10 22:56:23 +0530

    added data source attribute

--- pollution_data_class.py
+++ pollution_data_class.py
@@ -3,16 +3,17 @@ import time
 
 #class definatons to be followed by all different wesbite scripts
 class pollution_data:
-	def __init__(self,location,pm25 = 0,pm10 = 0,aqi=-999):#adopting us embassy aqi default non functioning value equivalent as -999
+	def __init__(self,location,pm25 = 0,pm10 = 0,aqi=-999,data_source = "NA"):#adopting us embassy aqi default non functioning value equivalent as -999
 		self.location = location
 		self.data_time = time.time()
 		self.pm10 = pm10
 		self.pm25 = pm25
 		self.aqi = aqi
+		self.data_source = data_source
 	
 
 	def describe(self):
-		print self.location, self.data_time , self.pm25 , self.pm10 , self.aqi
+		print self.location, self.data_time , self.pm25 , self.pm10 , self.aqi , self.data_source
 
 
 
